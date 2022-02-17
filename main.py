8c33db28cd785f827c73e63de76e1e7fb3329353
silentkiller365 <93775626+silentkiller365@users.noreply.github.com>
2022-02-17 20:20:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import telebot
 bot = telebot.TeleBot("5149509357:AAGbNSL98Q7YdFJbvs5KWH5WaHUQ33Wtoc8")
 
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["/start"])
 def send_welcome(message):
   bot.reply_to(message, "Hello! I'm SLBOTINFOBOT")
 
 
-@bot.message_handler(commands=["info"])
+@bot.message_handler(commands=["/info"])
 def send_message(message):
   bot.send_message(message.chat.id, "contact me @sitedown")
 
